199b1e242c507f1361b56afd9b324f0aab0811e6
highjeans <highjeans11@hotmail.com>
2023-09-12 10:39:42 +0100

    Implement Languages page

--- src/app/languages/page.js
+++ src/app/languages/page.js
@@ -1,9 +1,33 @@
 const metadata = {
-  title: "Unimplemented",
+  title: "Languages",
 }
 
-export default function Unimplemented() {
+export default function Languages() {
   return (
-    <p className="mt-10">This page is unimplemented.</p>
+    <div className="flex flex-col items-center w-full">
+      <p className="mt-10">Below is a table of the different language spoken in Taiwan.</p>
+      <table className="table-auto border-collapse mt-5 w-3/5">
+        <thead>
+          <tr>
+            <th>Languages</th>
+            <th>Percent of use at home</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Taiwanese Mandarin</td>
+            <td>83.5%</td>
+          </tr>
+          <tr>
+            <td>Taiwanese Hokkien</td>
+            <td>81.9%</td>
+          </tr>
+          <tr>
+            <td>Taiwanese Hakka</td>
+            <td>6.6%</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }

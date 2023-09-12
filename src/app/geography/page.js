0838452b00ee2fcd6fd58b125a63c6d40209629e
highjeans <highjeans11@hotmail.com>
2023-09-12 16:32:59 +0100

    Implement Geography page

--- src/app/geography/page.js
+++ src/app/geography/page.js
@@ -1,9 +1,14 @@
+import Image from 'next/image'
+
 const metadata = {
-  title: "Unimplemented",
+  title: "Geography",
 }
 
-export default function Unimplemented() {
+export default function Geography() {
   return (
-    <p className="mt-10">This page is unimplemented.</p>
+    <div className="flex items-center justify-center mt-0.5 px-5 p-10">
+      <p className="p-5">Taiwan has many mountains in the east, with slight sloping plains in the west.<br/>The diagram shows the terrain of Taiwan.</p>
+      <Image src="/Taiwan-Geography.jpeg" alt="Terrain of Taiwan" width={494} height={701}/>
+    </div>
   )
 }

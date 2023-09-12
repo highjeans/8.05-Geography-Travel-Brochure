ef7c6cc92c027df2cafaa28dc3828104ae66094a
highjeans <highjeans11@hotmail.com>
2023-09-12 14:38:50 +0100

    Implement Climate page

--- src/app/climate/page.js
+++ src/app/climate/page.js
@@ -1,9 +1,14 @@
+import Image from 'next/image'
+
 const metadata = {
-  title: "Unimplemented",
+  title: "Climate",
 }
 
-export default function Unimplemented() {
+export default function Climate() {
   return (
-    <p className="mt-10">This page is unimplemented.</p>
+    <div className="flex justify-center items-center mt-10 w-full px-16">
+      <p className="w-3/5 text-lg">Different parts of Taiwan have slightly different climates. Northern Taiwan has a humid, subtropical climate, with temperatures varying between seasons. Central and a portion of south Taiwan have a tropical monsoon climate where temperatures vary from warm to hot. You can find more accurate climates on the diagram shown here.</p>
+      <Image src="/Taiwan-Climate.png" alt="Taiwan's Climate Diagram" width={498} height={480} className="border"/>
+    </div>
   )
 }

c2c275d937bf554d7a758a802ff2076267b202d2
highjeans <highjeans11@hotmail.com>
2023-09-12 09:04:40 +0100

    Update home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <div className="w-full flex flex-col items-center mt-10">
       <Image src="/Taiwan-Home.jpeg" alt="Taiwan" width={548} height={318}/>
-      <p className="mt-3">
+      <p className="mt-3 text-center">
         Welcome to the country of Taiwan!
+        <br/>
+        Use the menu on the left to learn everything you need to know about Taiwan!
       </p>
     </div>
   )

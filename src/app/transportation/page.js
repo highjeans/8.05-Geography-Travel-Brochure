52b967834caaeb6cb810fc0fe81ed113d035410a
highjeans <highjeans11@hotmail.com>
2023-09-13 11:43:48 +0100

    Implement Transportation page

--- src/app/transportation/page.js
+++ src/app/transportation/page.js
@@ -1,9 +1,9 @@
 const metadata = {
-  title: "Unimplemented",
+  title: "Transportation",
 }
 
-export default function Unimplemented() {
+export default function Transportation() {
   return (
-    <p className="mt-10">This page is unimplemented.</p>
+    <p className="mt-10 px-20">Taiwan has many forms of transportation, including its highways and roads, buses, trams, railroads, and its High-speed rail. The high-speed rail can take you from the Taipei to Kaohsiung in 90 minutes.</p>
   )
 }

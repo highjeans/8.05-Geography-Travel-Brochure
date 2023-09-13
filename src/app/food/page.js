8246a0920db090c3eb2cae8b9a03e8b4adeb12e9
highjeans <highjeans11@hotmail.com>
2023-09-13 10:20:20 +0100

    Implement Food page

--- src/app/food/page.js
+++ src/app/food/page.js
@@ -1,9 +1,20 @@
 const metadata = {
-  title: "Unimplemented",
+  title: "Food",
 }
 
-export default function Unimplemented() {
+export default function Food() {
   return (
-    <p className="mt-10">This page is unimplemented.</p>
+    <div className="mt-10 w-full px-10 flex flex-col items-center">
+      <p>Taiwan has a lot of famous food, all listed below:</p>
+      <br/>
+      <ul className="list-disc list-inside w-fit text-left">
+        <li>Beef Noodle Soup</li>
+        <li>Marinated Pork Rice</li>
+        <li>Bubble Tea (Boba)</li>
+        <li>Gua Bao</li>
+        <li>Xiao Long Bao</li>
+        <li>And many more!</li>
+      </ul>
+    </div>
   )
 }

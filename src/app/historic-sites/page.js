4924ced27dd5703e6ee7fa6e60136381202bf8c4
highjeans <highjeans11@hotmail.com>
2023-09-13 10:53:47 +0100

    Implement Historic Sites page

--- src/app/historic-sites/page.js
+++ src/app/historic-sites/page.js
@@ -1,9 +1,19 @@
 const metadata = {
-  title: "Unimplemented",
+  title: "Historic Sites and Landmarks",
 }
 
-export default function Unimplemented() {
+export default function HistoricSites() {
   return (
-    <p className="mt-10">This page is unimplemented.</p>
+    <div className="mt-10 w-full px-10 flex flex-col items-center">
+      <p>Below is a list of a few Historic Sites and Landmarks in Taiwan:</p>
+      <br/>
+      <ul className="list-disc list-inside w-fit text-left">
+        <li>Longshan Temple</li>
+        <li>Chimei Museum</li>
+        <li>National Taichung Theater</li>
+        <li>Fort Zeelandia</li>
+        <li>Taipei Confucius Temple</li>
+      </ul>
+    </div>
   )
 }

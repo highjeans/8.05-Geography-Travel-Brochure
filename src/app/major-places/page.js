c579747e49e57e4dbac1d96de5e1e4e0344aeda2
highjeans <highjeans11@hotmail.com>
2023-09-13 09:13:07 +0100

    Implement Major Places page

--- src/app/major-places/page.js
+++ src/app/major-places/page.js
@@ -1,9 +1,19 @@
 const metadata = {
-  title: "Unimplemented",
+  title: "Major Places",
 }
 
-export default function Unimplemented() {
+export default function MajorPlaces() {
   return (
-    <p className="mt-10">This page is unimplemented.</p>
+    <div className="mt-10 w-full px-10 flex flex-col items-center">
+      <p>Below is a list of major cities in Taiwan:</p>
+      <br/>
+      <ul className="list-disc list-inside w-fit text-left">
+        <li>Taipei</li>
+        <li>Kaohsiung</li>
+        <li>Taichung</li>
+        <li>Tainan</li>
+        <li>New Taipei</li>
+      </ul>
+    </div>
   )
 }
